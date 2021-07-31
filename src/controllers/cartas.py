db167f0b221b1bbfef20d9c8011b0b52cd271dc4
gabrieltsbarbosa <gabrieltsbarbosa@hotmail.com>
2021-07-31 18:37:55 -0300

    Processos de bd movidos para src/repositories/cacardRepositories

--- src/controllers/cartas.py
+++ src/controllers/cartas.py
@@ -2,54 +2,27 @@ from flask import Flask, request
 from flask_restx import Api, Resource
 
 from src.server.instance import server
+from src.repositories.cardRepository import Repo
 
 app, api = server.app, server.api
 
-cartas_db = {}
 
 @api.route('/cartas')
 class Cartas(Resource):
     @app.route('/cartas/<int:id>', methods=['GET'])
     def getById(id):
-        for i in cartas_db:
-            if i == id:
-                return cartas_db[id], 200
-
-        return "", 404
+        return Repo.catch(id)
 
     def get(self, ):
-        cartas = []
-        for i in cartas_db:
-            cartas.append(cartas_db[i])
-        return cartas
+        return Repo.catch()
 
     def post(self, ):
-        response = api.payload
-        id = len(cartas_db) + 1
-        carta = {
-            id:{
-                'id': id,
-                'nome': response['nome'],
-                'endereco': response['endereco'],
-                'texto':response['texto']
-            }
-        }
-        cartas_db.update(carta)
-        return carta[id], 201
+        return Repo.insert(api.payload)
 
     @app.route('/cartas/<int:id>', methods=['PUT'])
     def put(id):
-        response = api.payload
-        carta = cartas_db[id]
-        carta[id][response['item']] = response['content']
-        return response, 200
+        return Repo.update(id, api.payload)
     
     @app.route('/cartas/<int:id>', methods=['DELETE'])
     def delete(id):
-        try:
-            response = api.payload
-            cartas_db.pop(int(id))
-            return response, 200
-
-        except:
-            return "Error", 404
+        return Repo.delete(id)
